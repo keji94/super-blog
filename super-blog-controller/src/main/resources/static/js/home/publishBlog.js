2c27375da15f244e62c4844277c48e70d9b81d1a
keji <798631828@qq.com>
2018-05-04 14:45:25 +0800

    博客发布页面初始化

--- super-blog-controller/src/main/resources/static/js/home/publishBlog.js
+++ super-blog-controller/src/main/resources/static/js/home/publishBlog.js
@@ -1,9 +1,15 @@
 $(function () {
-    $("#test").on('input', function () {
-        console.log($("#test").val());
+
+
+    layui.use(['form'], function(){
+        var form = layui.form;
+
+        //监听提交
+        form.on('submit(demo1)', function(data){
+            layer.alert(JSON.stringify(data.field), {
+                title: '最终的提交信息'
+            });
+            return false;
+        });
     });
-
 });
-
-
-
